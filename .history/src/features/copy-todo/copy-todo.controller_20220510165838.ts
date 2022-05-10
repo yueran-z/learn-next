0877bda8e5d0fa356d6c32e1d6975a28b8c32bab
yueran-z <yueranzhu@163.com>
2022-05-10 21:51:10 +0800

    todo

--- .history/src/features/copy-todo/copy-todo.controller_20220510165838.ts
+++ .history/src/features/copy-todo/copy-todo.controller_20220510165838.ts
@@ -1,12 +1,17 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { TodoService } from '../todo/todo.service';
 
-@Controller('copy-todo')
+@Controller('copy-todos')
 export class CopyTodoController {
+
     constructor(
         private readonly todoService: TodoService
-        ){}
+    ) { }
 
     @Post()
-    
+    create(@Body() body: { id: number, title: string, description: string }) {
+        this.todoService.createTodo(body);
+        return body;
+    }
+
 }
